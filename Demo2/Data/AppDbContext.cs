7319a0ecc7922353dc3a6f0aa08188d0258914e1
Admin <Admin@D20804>
2026-03-19 16:17:52 +0700

    Add project files.

---
+++ Demo2/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Demo2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor này bắt buộc phải có để nhận Connection String từ Program.cs
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+        public DbSet<Student> Students { get; set; }
+    }
+}
